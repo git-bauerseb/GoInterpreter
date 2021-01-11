a5cd64c1fa54812d341520d9e9d78740188d9ef8
git-bauerseb <48291831+git-bauerseb@users.noreply.github.com>
2021-01-11 15:12:40 +0100

    Implementation of the Lexer

---
+++ src/lexer/lexer_test.go
@@ -0,0 +1,89 @@
+package lexer
+
+import (
+	"testing"
+	"token"
+)
+
+func TestNextToken(t *testing.T) {
+	input :=
+		`let five = 5;
+		let ten = 10;
+		let add = fn(x, y) {
+		x + y;
+		};
+		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;
+		`
+
+	tests := []struct {
+		expectedType  token.TokenType
+		expectedValue string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.L_PAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.R_PAREN, ")"},
+		{token.L_BRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.R_BRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.L_PAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.R_PAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - Type wrong. Expected: %q, Got: %q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedValue {
+			t.Fatalf("tests[%d] - Literal wrong. Expected: %q, Got: %q",
+				i, tt.expectedValue, tok.Literal)
+		}
+	}
+}
